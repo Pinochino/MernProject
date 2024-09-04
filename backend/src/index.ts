db218b51e2338d6769180dbade8537ca0c6a2ee0
Pinochino <tranhunghp22112004@gmail.com>
2024-09-04 09:52:40 +0700

    Form default behavior

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,9 @@ import path from 'path';
 const port = 3000;
 const app = express();
 
-
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Http Logger
 app.use(morgan('dev'));
@@ -23,7 +23,6 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
 console.log(path.join(__dirname, 'resources/views'));
-
 
 app.get('/', (req: Request, res: Response) => {
   res.render('home');
---
+++ backend/src/model/game.ts
@@ -0,0 +1,7 @@
+import {ObjectId} from 'mongodb';
+
+class Game {
+    constructor(public name: string, public price: number, public category: string, public id?: ObjectId) {
+        
+    }
+}
